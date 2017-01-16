901a8e32417ad45ab4710324fffd5ffc0c6243e4
p2p-education <peer2peer.education@gmail.com>
2017-01-17 07:54:09 +0900

    automatically pushed

--- GL/anime/anime.hs
+++ GL/anime/anime.hs
@@ -4,15 +4,15 @@ import Bindings
 
 main :: IO ()
 main = do
-  (_progName, _args) <- getArgsAndInitialize
-  initialDisplayMode $= [WithDepthBuffer, DoubleBuffered]
-  _window <- createWindow "Hello World"
-  reshapeCallback $= Just reshape
-  depthFunc $= Just Less -- the comparison function for depth the buffer
-  angle <- newIORef 0
-  delta <- newIORef 0.1
-  pos <- newIORef (0, 0)
-  keyboardMouseCallback $= Just (keyboardMouse delta pos)
-  idleCallback $= Just (idle angle delta)
-  displayCallback $= display angle pos
-  mainLoop
+    (_progName, _args) <- getArgsAndInitialize
+    initialDisplayMode $= [WithDepthBuffer, DoubleBuffered]
+    _window <- createWindow "Hello World"
+    reshapeCallback $= Just reshape
+    depthFunc $= Just Less -- the comparison function for depth the buffer
+    angle <- newIORef 0
+    delta <- newIORef 0.1
+    pos <- newIORef (0, 0)
+    keyboardMouseCallback $= Just (keyboardMouse delta pos)
+    idleCallback $= Just (idle angle delta)
+    displayCallback $= display angle pos
+    mainLoop
---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Usage 
+
+
+## Install Haskell Platform
+
+## Install git
+
+## Download this repo and build
+
+~~~
+$ git clone http://www.github.com/p2p-education/haskell
+$ cd haskell && cd GL 
+$ make clean && ghc arrow.hs
+~~~
+
+## execution
+
+just type
+
+~~~
+$ ./arrow
+~~~
+
+if you are using Windows, execute 
+
+~~~
+arrow.exe
+~~~
+
